343526127544a8084163c49a914ea5a59c8ed37b
JuliaKudryavtseva <julia.kudr.alex@gmail.com>
2023-11-16 21:52:39 +0300

    reduce vgg16 feature map

--- model_vgg16.py
+++ model_vgg16.py
@@ -148,15 +148,15 @@ class SSD_VGG16(nn.Module):
         return loc_s, conf_s
 
 if __name__ == '__main__':
-    num_bboxes_s = [6, 6, 6, 6, 6, 6]
+    num_bboxes_s = [6, 6, 6, 6, 4, 4]
     
     model = SSD_VGG16(num_bboxes_s, 3)
     model.eval()
      
-    input_data  = torch.randn(1,3,720, 1280, dtype=torch.float, requires_grad=False)
+    input_data  = torch.randn(1,3,360, 640, dtype=torch.float, requires_grad=False)
     output_locs, output_confs = model( input_data )
     
-    model_graph  = draw_graph(model, input_size=(1,3,720,1280), expand_nested=True)
+    model_graph  = draw_graph(model, input_size=(1,3,360,640), expand_nested=True)
     visual_graph = model_graph.visual_graph
     graph_svg = visual_graph.pipe(format='png')
     with open('output_vgg16.png', 'wb') as f:
