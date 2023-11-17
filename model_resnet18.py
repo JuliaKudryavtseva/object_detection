cf56e249d88e4d85d17239dd3e2d8359e6116266
JuliaKudryavtseva <julia.kudr.alex@gmail.com>
2023-11-18 00:00:14 +0300

    model_resnet18.py

--- model_resnet18.py
+++ model_resnet18.py
@@ -1,4 +1,4 @@
-from torchvision.models import VGG16_Weights
+from torchvision.models import ResNet18_Weights
 from torchvision        import models
 
 from torchview import draw_graph
@@ -8,114 +8,78 @@ import torch.nn      as nn
 import numpy         as np
 
 import torch
+import copy
 
-class L2Norm(nn.Module):
-    def __init__(self, n_channels, scale):
-        super(L2Norm, self).__init__()
-        self.n_channels = n_channels
-        self.gamma      = scale or None
-        self.eps        = 1e-10
-        self.weight     = nn.Parameter(torch.Tensor(self.n_channels))
-        self.reset_parameters()
 
-    def reset_parameters(self):
-        init.constant_(self.weight, self.gamma)
-
-    def forward(self, x):
-        norm = torch.sqrt(x.pow(2).sum(dim=1, keepdim=True)) + self.eps
-        x = torch.div(x, norm)
-        x = self.weight.unsqueeze(0).unsqueeze(2).unsqueeze(3).expand_as(x) * x
-        return x
-
-class SSD_VGG16(nn.Module):
-    def __init__(self, num_bboxes_s, num_labels = 3):
+class SSD_resnet18(nn.Module):
+    def __init__(self, num_bboxes_s, num_labels=3):
         super().__init__()
-        
+
+        self.num_labels = num_labels
         self.num_bboxes_s = num_bboxes_s
-        self.num_labels   = num_labels
-        
-        self.used_layer_id_s       = [21, 33, 37, 41, 45, 49]
-        self.norm_layer            = L2Norm(512, 20)
-        
+
+        self.used_layer_id_s = list(range(7, 13))
+        self.num_bboxes_s = [6, 6, 6, 6, 6, 6]
+                
         base_layers       = self._build_base_layers ()
         extra_layers      = self._build_extra_layers()
         self.total_layers = base_layers + extra_layers
-        
+
         self.conf_layers, self.loc_layers = self._build_conf_loc_layers()
-    
+        
     def _build_base_layers(self):
-        backbone_model    = models.vgg16(weights=VGG16_Weights.DEFAULT)  #False
-        
-        base_layers = nn.ModuleList(list(backbone_model.features)[:-1])
-        base_layers[16].ceil_mode = True
-        
-        pool5 = nn.MaxPool2d(kernel_size=3, stride=1, padding=1)          
-        conv6 = nn.Conv2d( 512, 1024, kernel_size=3, padding=6, dilation=6)
-        relu6 = nn.ReLU(inplace=True)
-        conv7 = nn.Conv2d(1024, 1024, kernel_size=1)
-        relu7 = nn.ReLU(inplace=True)
+        backbone_model    = models.resnet18()  #False
 
-        nn.init.xavier_uniform_(conv6.weight)
-        nn.init.zeros_         (conv6  .bias)
-        nn.init.xavier_uniform_(conv7.weight)
-        nn.init.zeros_         (conv7  .bias)
-       
-        base_layers.extend( [pool5, conv6, relu6, conv7, relu7] )
-        
-        return base_layers
+        drop_layers = ['layer4', 'avgpool', 'fc']
+        base_layers = [layer for name, layer in backbone_model.named_children() if name not in drop_layers]
 
-    def _build_extra_layers(self):
+        conv256 = nn.Conv2d(256, 128, kernel_size=3, stride=1, padding=1)
+
+        init.xavier_uniform_(conv256.weight)
+        init.zeros_         (conv256  .bias)
+
+
+        basic_layer = copy.deepcopy(getattr(backbone_model, 'layer2'))
+        basic_layer[0].conv1 = conv256
+        basic_layer[0].downsample[0] = copy.deepcopy(conv256)
+        base_layers.append(basic_layer)
+
+        return nn.ModuleList(base_layers)
+
+    def _build_extra_layers(self, number_blocks=5):
         extra_layers = []
+
+        conv1 = nn.Conv2d(128, 64, kernel_size=1, stride=1            )
+        relu1 = nn.ReLU(inplace=True)
+        conv2 = nn.Conv2d(64, 128, kernel_size=3, stride=2, padding=1 )
+        relu2 = nn.ReLU(inplace=True)
         
-        conv8_1  = nn.Conv2d( 1024, 256, kernel_size=1, stride=1           )
-        relu8_1  = nn.ReLU(inplace=True)
-        conv8_2  = nn.Conv2d( 256, 512, kernel_size=3, stride=2, padding=1)
-        relu8_2  = nn.ReLU(inplace=True)
-        conv9_1  = nn.Conv2d( 512, 128, kernel_size=1, stride=1           )
-        relu9_1  = nn.ReLU(inplace=True)
-        conv9_2  = nn.Conv2d( 128, 256, kernel_size=3, stride=2, padding=1)
-        relu9_2  = nn.ReLU(inplace=True)
-        conv10_1 = nn.Conv2d( 256, 128, kernel_size=1, stride=1           )
-        relu10_1 = nn.ReLU(inplace=True)
-        conv10_2 = nn.Conv2d( 128, 256, kernel_size=3, stride=1           )
-        relu10_2 = nn.ReLU(inplace=True)
-        conv11_1 = nn.Conv2d( 256, 128, kernel_size=1                     )
-        relu11_1 = nn.ReLU(inplace=True)
-        conv11_2 = nn.Conv2d( 128, 256, kernel_size=3, stride=1           )
-        relu11_2 = nn.ReLU(inplace=True)
+        init.xavier_uniform_(conv1 .weight)
+        init.zeros_         (conv1 .bias  )
+        init.xavier_uniform_(conv2 .weight)
+        init.zeros_         (conv2 .bias  )
         
-        nn.init.xavier_uniform_(conv8_1 .weight)
-        nn.init.zeros_         (conv8_1 .bias  )
-        nn.init.xavier_uniform_(conv8_2 .weight)
-        nn.init.zeros_         (conv8_2 .bias  )
-        nn.init.xavier_uniform_(conv9_1 .weight)
-        nn.init.zeros_         (conv9_1 .bias  )
-        nn.init.xavier_uniform_(conv9_2 .weight)
-        nn.init.zeros_         (conv9_2 .bias  )
-        nn.init.xavier_uniform_(conv10_1.weight)
-        nn.init.zeros_         (conv10_1.bias  )
-        nn.init.xavier_uniform_(conv10_2.weight)
-        nn.init.zeros_         (conv10_2.bias  )
-        nn.init.xavier_uniform_(conv11_1.weight)
-        nn.init.zeros_         (conv11_1.bias  )
-        nn.init.xavier_uniform_(conv11_2.weight)
-        nn.init.zeros_         (conv11_2.bias  )
-        
-        extra_layers = nn.ModuleList( [conv8_1, relu8_1, conv8_2, relu8_2, conv9_1, relu9_1, conv9_2, relu9_2, conv10_1, relu10_1, conv10_2, relu10_2, conv11_1, relu11_1, conv11_2, relu11_2] )
-        return extra_layers
+        extra_layers = nn.Sequential(conv1, relu1, conv2, relu2)
+
+        return [extra_layers for _ in range(number_blocks)]
     
     def _build_conf_loc_layers(self):
-        out_channels_s = [ self.total_layers[i].out_channels for i in self.used_layer_id_s ] 
         
         conf_layers, loc_layers = [], []
         for i, j in enumerate(self.used_layer_id_s):
-            conf_layer = nn.Conv2d( self.total_layers[j].out_channels, self.num_bboxes_s[i] * self.num_labels, kernel_size=3, padding=1)
-            loc_layer  = nn.Conv2d( self.total_layers[j].out_channels, self.num_bboxes_s[i] * 4              , kernel_size=3, padding=1)
+
+            if j > 7:
+                _out_channels = self.total_layers[j][2].out_channels
+            else:
+                _out_channels =  self.total_layers[j][1].conv2.out_channels
+
+            conf_layer = nn.Conv2d( _out_channels, self.num_bboxes_s[i] * self.num_labels, kernel_size=3, padding=1)
+            loc_layer  = nn.Conv2d( _out_channels, self.num_bboxes_s[i] * 4              , kernel_size=3, padding=1)
             
-            nn.init.xavier_uniform_(conf_layer.weight)
-            nn.init.zeros_         (conf_layer  .bias)
-            nn.init.xavier_uniform_(loc_layer .weight)
-            nn.init.zeros_         (loc_layer   .bias)
+            init.xavier_uniform_(conf_layer.weight)
+            init.zeros_         (conf_layer  .bias)
+            init.xavier_uniform_(loc_layer .weight)
+            init.zeros_         (loc_layer   .bias)
             
             conf_layers += [conf_layer]
             loc_layers  += [loc_layer ]
@@ -124,18 +88,15 @@ class SSD_VGG16(nn.Module):
         loc_layers  = nn.ModuleList(loc_layers )
         
         return conf_layers, loc_layers
-
-    def forward(self, x, verbose=False):
+    
+    def forward(self, x):
         source_s, loc_s, conf_s = [], [], []
         
-        for i, current_layer in enumerate(self.total_layers, -1):
+        for i, current_layer in enumerate(self.total_layers):
             x = current_layer(x)
-            if i in self.used_layer_id_s:
-                if i == 21:
-                    s = self.norm_layer(x)
-                else:
-                    s = x
-                source_s.append(s)
+            if i > 6:
+                source_s.append(x)
+                
         for s, l, c in zip(source_s, self.loc_layers, self.conf_layers):
             conf_s.append(c(s).permute(0, 2, 3, 1).contiguous())
             loc_s .append(l(s).permute(0, 2, 3, 1).contiguous())
@@ -147,19 +108,20 @@ class SSD_VGG16(nn.Module):
 
         return loc_s, conf_s
 
+
 if __name__ == '__main__':
-    num_bboxes_s = [6, 6, 6, 6, 4, 4]
+    num_bboxes_s = [6, 6, 6, 6, 6, 6]
     
-    model = SSD_VGG16(num_bboxes_s, 3)
+    model = SSD_resnet18(num_bboxes_s, 3)
     model.eval()
      
-    input_data  = torch.randn(1,3,360, 640, dtype=torch.float, requires_grad=False)
+    input_data  = torch.randn(1, 3, 720, 1280, dtype=torch.float, requires_grad=False)
     output_locs, output_confs = model( input_data )
     
-    model_graph  = draw_graph(model, input_size=(1,3,360,640), expand_nested=True)
+    model_graph  = draw_graph(model, input_size=(1, 3, 720, 1280), expand_nested=True)
     visual_graph = model_graph.visual_graph
     graph_svg = visual_graph.pipe(format='png')
-    with open('output_vgg16.png', 'wb') as f:
+    with open('draft_output_resnet18.png', 'wb') as f:
         f.write(graph_svg)
 
     print(output_locs .shape)
